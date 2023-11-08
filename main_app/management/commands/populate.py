dc90633f167fa1bec45aa88d3a461d0c4df742a1
LEE MUTAI <71839051+leemutai@users.noreply.github.com>
2023-11-08 15:19:09 +0300

    Added Populate, command, update profile

---
+++ main_app/management/commands/populate.py
@@ -0,0 +1,7 @@
+from django.core.management import BaseCommand
+
+
+class Command(BaseCommand):
+    help = "Populates employees table with 1000 records"
+    def handle(self, *args, **options):
+        pass
--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,17 @@
+import os.path
+import uuid
+
 from django.db import models
+
+
+def unique_img_name(instance, filename):
+    name = uuid.uuid4()
+    print(name)
+    # jehhhhs.png ["xxxxxx", "ugfgf"]
+    ext = filename.split(".")[-1]
+    full_name = f"{name}.{ext}"  # dhfgshgs.png
+    # full_name = "%s.%s" % (name, ext) employees/dhsjdsj.png
+    return os.path.join('employees', full_name)
 
 
 # Create your models here.
@@ -9,7 +22,7 @@ class Employee(models.Model):
     dob = models.DateField(null=True)
     salary = models.DecimalField(max_digits=7, decimal_places=2)  # 67899.90
     disabled = models.BooleanField(default=False)
-    profile = models.ImageField(upload_to="employees", null=True)
+    profile = models.ImageField(upload_to=unique_img_name, null=True, default="employees/employee.png")
     created_at = models.DateTimeField(auto_now_add=True, null=True)
     update_at = models.DateTimeField(auto_now=True, null=True)
 
@@ -18,4 +31,7 @@ class Employee(models.Model):
 
 # python manage.py makemigrations
 # python manage.py migrate
+# python manage.py populate
 # pip install Pillow
+
+# module package library in python
